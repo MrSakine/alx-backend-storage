49bb317821432f9a033b599578ccdd08a58742c0
Flyer <115190839+MrSakine@users.noreply.github.com>
2024-05-09 16:05:50 +0000

    Update 100-average_weighted_score.sql

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -7,7 +7,6 @@ BEGIN
 	average_score = (
     SELECT
     SUM(corrections.score * projects.weight) / SUM(projects.weight)
-    AS average
     FROM corrections 
     INNER JOIN projects
     WHERE corrections.user_id = user_id
