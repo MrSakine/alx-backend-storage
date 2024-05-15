e89910ea6f787e06834a20d9d97d3490d2669d07
“MrSakine” <maskine20@gmail.com>
2024-05-15 11:10:58 +0000

    Storing lists

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -20,6 +20,21 @@ def count_calls(method: Callable) -> Callable:
     return wrapper
 
 
+def call_history(method: Callable) -> Callable:
+    """Storing lists"""
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """Wrapper content"""
+        key = method.__qualname__
+        inp = key + ":inputs"
+        out = key + ":outputs"
+        res = method(self, *args)
+        self._redis.rpush(inp, *args)
+        self._redis.rpush(out, res)
+        return res
+    return wrapper
+
+
 class Cache():
     def __init__(self):
         """Initialize redis instance"""
@@ -27,6 +42,7 @@ class Cache():
         self._redis.flushdb()
 
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes,  int,  float]) -> str:
         """
         Generate a random key using uuid,
