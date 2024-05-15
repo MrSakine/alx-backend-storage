35a094482623b9cba24e51fe0637f5c928dc913e
“MrSakine” <maskine20@gmail.com>
2024-05-15 10:30:44 +0000

    Reading from Redis and recovering original type

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,6 +3,7 @@
 import typing
 import uuid
 import redis
+from typing import Union, Optional, Callable, Any
 
 
 class Cache():
@@ -11,7 +12,7 @@ class Cache():
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: typing.Union[str, bytes,  int,  float]) -> str:
+    def store(self, data: Union[str, bytes,  int,  float]) -> str:
         """
         Generate a random key using uuid,
         store the input data in Redis using the random key
@@ -20,3 +21,25 @@ class Cache():
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> Any:
+        """Reading from Redis and recovering original type"""
+        element = self._redis.get(key)
+        if not element:
+            return None
+        elif fn is int:
+            return self.get_int(element)
+        elif fn is str:
+            return self.get_str(element)
+        elif callable(fn):
+            return fn(element)
+        else:
+            return element
+
+    def get_str(self, data: bytes) -> str:
+        """Converts bytes to string"""
+        return data.decode('utf-8')
+
+    def get_int(self, data: bytes) -> int:
+        """Converts bytes to integers"""
+        return int(data)
